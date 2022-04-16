a5186528786550e0a813930656d20bc910154b6f
choewy <choewy32@gmail.com>
2022-04-16 14:53:50 +0900

    refactor

--- server/src/like/like.service.js
+++ server/src/like/like.service.js
@@ -4,17 +4,9 @@ const { Like } = require('../../models');
 
 const LikeService = {
   changeLike: async (user_id, post_id) => {
-    const like = await Like.findOne({
-      where: { user_id, post_id },
-    });
-
-    if (like) {
-      return await Like.destroy({
-        where: { user_id, post_id },
-      });
-    }
-
-    await Like.create({ user_id, post_id });
+    const target = { user_id, post_id };
+    const like = await Like.findOne({ where: target });
+    like ? await Like.destroy({ where: target }) : Like.create(target);
   },
 };
 
